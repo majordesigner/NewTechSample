8c9207b03e3c79504f1be9e6439455e9bb3939c8
Major Designer <hsngcmn@gmail.com>
2017-08-29 22:23:01 +0300

    grunt less watch

--- NewTechSample.Web/gruntfile.js
+++ NewTechSample.Web/gruntfile.js
@@ -4,11 +4,41 @@
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        less: {
+            development: {
+                options: {
+                    paths: ['Content/css'],
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "Content/css/views/general.css": "Content/css/views/general.less" // destination file and source file
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['Content/css/views/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
     });
+
+    
+    grunt.event.on('watch', function (action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
+    
 
     // Load the plugin that provides the "uglify" task.
     //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    //grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['less', 'watch']);
 };
